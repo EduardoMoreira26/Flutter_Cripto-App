188bb15492d90a9bd5df0cc1354b9dba0050fa6f
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-08 13:55:51 -0300

    create provider

--- lib/repositories/favorites_repository.dart
+++ lib/repositories/favorites_repository.dart
@@ -1,6 +1,23 @@
+import 'dart:collection';
+
 import 'package:cripto/models/coin.dart';
 import 'package:flutter/material.dart';
 
 class FavoritesRepository extends ChangeNotifier {
   List<Coin> _list = [];
+
+  UnmodifiableListView<Coin> get list => UnmodifiableListView(_list);
+
+  saveAll(List<Coin> coins) {
+    coins.forEach((coin) {
+      if (!_list.contains(coin)) _list.add(coin);
+    });
+
+    notifyListeners();
+  }
+
+  remove(Coin coin) {
+    _list.remove(coin);
+    notifyListeners();
+  }
 }
