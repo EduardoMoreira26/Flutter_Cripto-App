aabbd5fe5d1affef41bfe334e6b07e45f4b9d39c
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-08 15:35:01 -0300

    style input

--- lib/pages/details_coin_page.dart
+++ lib/pages/details_coin_page.dart
@@ -1,5 +1,6 @@
 import 'package:cripto/models/coin.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 // ignore: must_be_immutable
 class DetailsCoinPage extends StatefulWidget {
@@ -15,23 +16,62 @@ class DetailsCoinPage extends StatefulWidget {
 }
 
 class _DetailsCoinPageState extends State<DetailsCoinPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final _form = GlobalKey<FormState>();
+  final _value = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text(widget.coin.name)),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                child: Image.asset(widget.coin.icon),
-                width: 50,
+      body: Padding(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image.asset(widget.coin.icon),
+                    width: 50,
+                  ),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.coin.price),
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+            Form(
+              key: _form,
+              child: TextFormField(
+                controller: _value,
+                style: TextStyle(fontSize: 22),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'Reais',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
