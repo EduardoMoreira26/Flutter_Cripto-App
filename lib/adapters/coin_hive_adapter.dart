4ddb0ac3d068766c5613261b5ba48e68875cb433
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-09 14:52:21 -0300

    create settings preferences

---
+++ lib/adapters/coin_hive_adapter.dart
@@ -0,0 +1,25 @@
+import 'package:cripto/models/coin.dart';
+import 'package:hive/hive.dart';
+
+class CoinHiveAdapter extends TypeAdapter<Coin> {
+  @override
+  final typeId = 0;
+
+  @override
+  Coin read(BinaryReader reader) {
+    return Coin(
+      icon: reader.readString(),
+      name: reader.readString(),
+      initials: reader.readString(),
+      price: reader.readDouble(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Coin obj) {
+    writer.writeString(obj.icon);
+    writer.writeString(obj.name);
+    writer.writeString(obj.initials);
+    writer.writeDouble(obj.price);
+  }
+}
--- lib/repositories/favorites_repository.dart
+++ lib/repositories/favorites_repository.dart
@@ -1,12 +1,29 @@
 import 'dart:collection';
-
+import 'package:cripto/adapters/coin_hive_adapter.dart';
 import 'package:cripto/models/coin.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class FavoritesRepository extends ChangeNotifier {
   List<Coin> _list = [];
+  LazyBox box;
 
- UnmodifiableListView<Coin> get list => UnmodifiableListView(_list);
+  FavoritesRepository() {
+    _startRepository();
+  }
+
+  _startRepository() async {
+    await _openBox();
+    await _readFavorites();
+  }
+
+  _openBox() async {
+    Hive.registerAdapter(CoinHiveAdapter());
+  }
+
+  _readFavorites() async {}
+
+  UnmodifiableListView<Coin> get list => UnmodifiableListView(_list);
 
   saveAll(List<Coin> coins) {
     coins.forEach((coin) {
