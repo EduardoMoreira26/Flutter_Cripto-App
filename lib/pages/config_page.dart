3b49513d211815ca30fe4c43294fc55427d8e777
Eduardo Moreira <Du_du.mba@hotmail.com>
2022-01-12 15:25:55 -0300

    config page

--- lib/pages/config_page.dart
+++ lib/pages/config_page.dart
@@ -1,6 +1,7 @@
 import 'package:cripto/configs/app_settings.dart';
 import 'package:cripto/repositories/account__reposiotry.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +16,11 @@ class _ConfigPageState extends State<ConfigPage> {
   @override
   Widget build(BuildContext context) {
     final conta = context.watch<AccountReposiotry>();
-    final loc = context.watch<AppSettings>().locale;
-    NumberFormat real = NumberFormat.currency(locale: loc['locale'], name: loc['name']);
+    final loc = context.read<AppSettings>().locale;
+    NumberFormat real =
+        NumberFormat.currency(locale: loc['locale'], name: loc['name']);
 
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Configurações'),
       ),
@@ -36,13 +38,53 @@ class _ConfigPageState extends State<ConfigPage> {
                 ),
               ),
               trailing:
-                  IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                  IconButton(onPressed: updateSaldo, icon: Icon(Icons.edit)),
             ),
             Divider(),
           ],
         ),
       ),
     );
+  }
 
+  updateSaldo() async {
+    final form = GlobalKey<FormState>();
+    final valor = TextEditingController();
+    final conta = context.read<AccountReposiotry>();
+
+    valor.text = conta.saldo.toString();
+
+    AlertDialog dialog = AlertDialog(
+      title: Text('Atualizar o Saldo'),
+      content: Form(
+        key: form,
+        child: TextFormField(
+          controller: valor,
+          keyboardType: TextInputType.number,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d*')),
+          ],
+          validator: (value) {
+            if (value.isEmpty) return 'Informe o valor do saldo';
+            return null;
+          },
+        ),
+      ),
+      actions: [
+        TextButton(
+            onPressed: () => Navigator.pop(context), child: Text('CANCELAR')),
+        TextButton(
+          onPressed: () {
+            if (form.currentState.validate()) {
+              conta.setSaldo(double.parse(valor.text));
+              Navigator.pop(context);
+            }
+          },
+          child: Text('SALVAR'),
+        ),
+      ],
+    );
+
+    showDialog(context: context, builder: (context) => dialog);
   }
 }
